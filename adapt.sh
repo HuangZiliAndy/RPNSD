c894d6360c174d10b8bd0f19ca865f9ade24cee5
zhuang <hzili1@b04.clsp.jhu.edu>
2020-01-20 15:14:01 -0500

    fix minor mistake in adapt.sh

--- adapt.sh
+++ adapt.sh
@@ -42,7 +42,7 @@ for fold_num in {1..5}; do
   mkdir -p $exp_dir/{model,log} || exit 1;
   
   ${cuda_cmd} $exp_dir/log/train_log \
-	  CUDA_VISIBLE_DEVICES=$free_gpu scripts/train.py $exp_dir $train_dir $dev_dir --cfg_file $cfg_file \
+	  CUDA_VISIBLE_DEVICES=$free_gpu python3 scripts/train.py $exp_dir $train_dir $dev_dir --cfg_file $cfg_file \
 	  --freeze $freeze --set_bn_fix $set_bn_fix --pretrain_model $pretrain_model \
   	  --epochs $epochs --batch_size $batch_size --num_workers $num_workers --optimizer $optimizer \
   	  --lr $lr --min_lr $min_lr --patience $patience --seed $seed \
